b8c4070da495e6dca300f0c9be2237ebf98064c5
fuenteadicto <fuenteadicto@gmail.com>
2024-03-04 21:07:37 +0100

    first commit

---
+++ src/main/java/com/fuenteadictos/fuenteadictos/FuenteadictosApplication.java
@@ -0,0 +1,13 @@
+package com.fuenteadictos.fuenteadictos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuenteadictosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuenteadictosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fuenteadictos/fuenteadictos/FuenteadictosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuenteadictos.fuenteadictos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuenteadictosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
